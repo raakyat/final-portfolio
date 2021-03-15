cef2b555a6de845ce502b802cb21fb999af8d6b9
github username <dirtyassweed@icloud.com>
2021-03-15 17:15:52 -0400

    updates projects

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { projects } from '../components/Projects'
-import parse from 'html-react-parser';
+import { projects } from '../components/Projects';
 
 const ProjectPage = (props) => {
     const index = props.match.params.id - 1;
-    const project = projects[index]
-    const github = project.github
+    const project = projects[index];
+    const github = project.github;
+    const demo = project.demo;
+    const logo = project.logo;
     
     return (
         <>
             <div className="project-container">
-                <img className="project-img" src={ project.logo } alt="img"/><br/>
-                {parse(
-                    project.demo
-                )}
-                <div className="proj-btns">
-                    <button onClick={ props.history.goBack }>
-                        Back
-                    </button>
-                    <h3>
-                        Stack:
-                    </h3>
-                    <button onClick={() => { window.open(github, "_blank") }}>
-                        Code    
-                    </button>
-                </div>
+                <img className="project-img" src={ logo } alt="img"/><br/>
                 <div className="technologies-container">
                     { project.technologies.map(tech => 
                         <div className="tech-div" key={tech}>
@@ -33,7 +20,18 @@ const ProjectPage = (props) => {
                             </p>
                         </div>
                     )}
-                </div><br/>
+                </div>
+                <div className="proj-btns">
+                    <button onClick={ props.history.goBack }>
+                        Back
+                    </button>
+                    <button onClick={() => { window.open(demo, "_blank") }}>
+                        Test Me!    
+                    </button>
+                    <button onClick={() => { window.open(github, "_blank") }}>
+                        Code    
+                    </button>
+                </div>
                 <p>
                     { project.description }
                 </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,60 @@
 export const projects = [
 
     {
+
         id: 1,
-        name: "Lab Locator",
-        logo: "https://i.ibb.co/Ykj9cZV/lab-Locator.jpg",
-        description: "Lab Locator is a platform built exclusively for Recording Engineers to advertise their available studio space. In addition, clients have the ability to leave feedback on their experience at a given studio.",
-        github: "https://github.com/Dylan-Williamson/lab-locator/",
-        demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/yedD6RtWNqs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
-        technologies: ["Ruby On Rails", "PostgreSQL", "HTML", "CSS", "React.JS", "Redux" ]
+        name: "Rendezvous",
+        logo: "https://i.ibb.co/3zyxKFP/Screen-Shot-2021-03-14-at-1-34-58-PM.png",
+        description: "Rendezvous is a messaging application designed for anyone with a valid Gmail account. Users can create their own channels to communicate with others all around the world.",
+        github: "https://github.com/Dylan-Williamson/rendezvous-chat",
+        demo: 'https://rendezvous-chat.web.app',
+        technologies: ["React.js", "Redux", "Firebase", "HTML", "CSS"]
+
     },
 
     {
 
         id: 2,
-        name: "LooKEY Monster",
-        logo: "https://i.ibb.co/sRnxxv3/Lookey-Monster.jpg",
-        description: "LooKEY Monster is a Command-Line-Interface application for searching metadata of any song on Spotify. This tool was developed for Music Producers to quickly find the Key, Tempo and more from a song with a simple search.",
-        github: "https://github.com/Dylan-Williamson/lookey_monster",
-        demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/bgHlgei8txA" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
-        technologies: ["Ruby"]
+        name: "Spore Loko",
+        logo: "https://i.ibb.co/gP1CvKv/Screen-Shot-2021-03-15-at-11-09-23-AM.png",
+        description: "Spore Loko is a marketplace for purchase premium quality mushroom spores in the United States. The application utilizes Stripe to process payments while maintaining maxiumum security.",
+        github: "https://github.com/Dylan-Williamson/spore-loko",
+        demo: 'https://spore-loko.web.app',
+        technologies: ["React.js", "Redux", "Node.js", "Firebase", "HTML", "CSS"]
 
     },
 
-    {
+    // {
+    //     id: 3,
+    //     name: "Lab Locator",
+    //     logo: "https://i.ibb.co/Ykj9cZV/lab-Locator.jpg",
+    //     description: "Lab Locator is a platform built exclusively for Recording Engineers to advertise their available studio space. In addition, clients have the ability to leave feedback on their experience at a given studio.",
+    //     github: "https://github.com/Dylan-Williamson/lab-locator/",
+    //     demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/yedD6RtWNqs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
+    //     technologies: ["Ruby On Rails", "PostgreSQL", "HTML", "CSS", "React.JS", "Redux" ]
+    // },
 
-        id: 3,
-        name: "MixMasters",
-        logo: "https://i.ibb.co/GRKqnF0/mmlogo.png",
-        description: "MixMasters is a freelance platform for Mixing and Mastering engineers to advertise their services. Users have the ability to upload services, purchase services as well as review orders and leave feedback.",
-        github: "https://github.com/Dylan-Williamson/mixmasters",
-        demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/WV9s7XFaFvU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
-        technologies: ["Ruby on Rails", "SQLite3", "HTML", "CSS"]
+    // {
 
-    }
+    //     id: 4,
+    //     name: "LooKEY Monster",
+    //     logo: "https://i.ibb.co/sRnxxv3/Lookey-Monster.jpg",
+    //     description: "LooKEY Monster is a Command-Line-Interface application for searching metadata of any song on Spotify. This tool was developed for Music Producers to quickly find the Key, Tempo and more from a song with a simple search.",
+    //     github: "https://github.com/Dylan-Williamson/lookey_monster",
+    //     demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/bgHlgei8txA" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
+    //     technologies: ["Ruby"]
+
+    // },
+
+    // {
+
+    //     id: 5,
+    //     name: "MixMasters",
+    //     logo: "https://i.ibb.co/GRKqnF0/mmlogo.png",
+    //     description: "MixMasters is a freelance platform for Mixing and Mastering engineers to advertise their services. Users have the ability to upload services, purchase services as well as review orders and leave feedback.",
+    //     github: "https://github.com/Dylan-Williamson/mixmasters",
+    //     demo: '<iframe width=100% height="350" src="https://www.youtube.com/embed/WV9s7XFaFvU" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>',
+    //     technologies: ["Ruby on Rails", "SQLite3", "HTML", "CSS"]
+
+    // }
 ]
